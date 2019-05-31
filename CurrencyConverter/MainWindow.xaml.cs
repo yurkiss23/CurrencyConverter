0328712f4c26ffd7e4780e97bd40cdec9ecb3cba
yurkiss <terranet23@gmail.com>
2019-05-31 10:26:47 +0300

    simple converter

--- CurrencyConverter/MainWindow.xaml.cs
+++ CurrencyConverter/MainWindow.xaml.cs
@@ -72,23 +72,82 @@ namespace CurrencyConverter
         {
             MessageBox.Show("Change");
             var currencyList = JsonConvert.DeserializeObject<List<CurrencyInfoModel>>(_responce);
+
+            if (!string.IsNullOrEmpty(txtChange.Text) && txtChange.Text.All(char.IsDigit))
+            {
+                switch (cbChange.SelectedIndex)
+                {
+                    case 0:
+                        double digit1 = Convert.ToDouble(txtChange.Text) / double.Parse(currencyList[0].buy.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+                        txtGet.Text = Convert.ToString(digit1);
+                        break;
+                    case 1:
+                        double digit2 = Convert.ToDouble(txtChange.Text) / double.Parse(currencyList[1].buy.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+                        txtGet.Text = Convert.ToString(digit2);
+                        break;
+                    case 2:
+                        double digit3 = Convert.ToDouble(txtChange.Text) / double.Parse(currencyList[2].buy.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+                        txtGet.Text = Convert.ToString(digit3);
+                        break;
+                }
+            }
+            else
+                txtGet.Text = "...";
+
             //foreach (var item in currencyList)
             //{
             //    MessageBox.Show(item.base_ccy + ":" + item.ccy + ":" + item.buy + ":" + item.sale);
             //}
-
-            foreach(var item in currencyList)
-            {
-                if((string)cbChange.SelectedValue==item.base_ccy || (string)cbChange.SelectedValue==item.ccy)
-                {
-                    if ((string)cbGet.SelectedValue == item.base_ccy || (string)cbGet.SelectedValue == item.ccy)
-                    {
-                        MessageBox.Show(cbChange.SelectedValue.ToString() + ":" + cbGet.SelectedValue.ToString());
-                        txtGet.Text = Math.Round((Decimal.Parse(txtChange.Text) / item.sale), 2, MidpointRounding.AwayFromZero).ToString();
-                    }
-                    return;
-                }
-            }
+            //MessageBox.Show(currencyList.Count().ToString());
+            //string condition = cbChange.SelectedValue.ToString() + ":" + cbGet.SelectedValue.ToString();
+            //MessageBox.Show(condition);
+            //foreach (var itemCng in currencyList)
+            //{
+            //    //MessageBox.Show("change");
+            //    //MessageBox.Show(cbChange.SelectedValue.ToString() + ":" + cbGet.SelectedValue.ToString());
+            //    if ((string)cbChange.SelectedValue==itemCng.base_ccy || (string)cbChange.SelectedValue==itemCng.ccy)
+            //    {
+            //        string left = ((string)cbChange.SelectedValue == itemCng.base_ccy) ? "base_ccy" : "ccy";
+            //        string change = ((string)cbChange.SelectedValue == itemCng.base_ccy) ? itemCng.base_ccy : itemCng.ccy;
+                    
+            //        foreach (var itemGet in currencyList)
+            //        {
+            //            MessageBox.Show(itemGet.base_ccy+":"+itemGet.ccy);
+            //            if ((string)cbGet.SelectedValue == itemGet.base_ccy || (string)cbGet.SelectedValue == itemGet.ccy)
+            //            {
+            //                string right = ((string)cbGet.SelectedValue == itemGet.base_ccy) ? "base_ccy" : "ccy";
+            //                string get = ((string)cbGet.SelectedValue == itemGet.base_ccy) ? itemGet.base_ccy : itemGet.ccy;
+                            
+            //                string cng = left + ":" + right;
+            //                MessageBox.Show(cng);
+            //                MessageBox.Show(change + ":" + get);
+            //                if ((change + ":" + get) == condition)
+            //                {
+            //                    switch (cng)
+            //                    {
+            //                        case "base_ccy:ccy":
+            //                            txtGet.Text = Math.Round((Decimal.Parse(txtChange.Text) / itemCng.sale), 2, MidpointRounding.AwayFromZero).ToString();
+            //                            break;
+            //                        case "base_ccy:base_ccy":
+            //                            decimal changer = Math.Round((Decimal.Parse(txtChange.Text) * itemCng.buy), 2, MidpointRounding.AwayFromZero);
+            //                            //decimal get = Math.Round((Decimal.Parse(txtGet.Text) / item.sale), 2, MidpointRounding.AwayFromZero);
+            //                            txtGet.Text = Math.Round(changer / itemCng.sale, 2, MidpointRounding.AwayFromZero).ToString();
+            //                            break;
+            //                        case "ccy:base_ccy":
+            //                            txtGet.Text = Math.Round((Decimal.Parse(txtChange.Text) * itemCng.buy), 2, MidpointRounding.AwayFromZero).ToString();
+            //                            break;
+            //                        case "ccy:ccy":
+            //                            break;
+            //                        default:
+            //                            break;
+            //                    }
+            //                }
+            //                //txtGet.Text = Math.Round((Decimal.Parse(txtChange.Text) / item.sale), 2, MidpointRounding.AwayFromZero).ToString();
+            //                return;
+            //            }
+            //        }
+            //    }
+            //}
         }
     }
 }
